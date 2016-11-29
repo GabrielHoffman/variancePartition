7577b948c036a78ade84d9a61fcb9cc03666b728
Gabriel Hoffman <gabriel.hoffman@mssm.edu>
2016-11-29 19:26:58 +0000

    v1.5.5
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/variancePartition@124580 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ESS.R
+++ R/ESS.R
@@ -6,7 +6,7 @@
 #' Compute effective sample size based on correlation structure in linear mixed model
 #'
 #' @param fit model fit from lmer()
-#' @param method "full" uses the full correlation structure of the model. "fast" is exact and can be much faster for > 500 samples. The "approximate" method makes the simplifying assumption that the study has a mean of m samples in each of k groups, and computes m based on the study design.  When the study design is evenly balanced (i.e. the assumption is met), this gives the same results as the "full" method.  
+#' @param method "full" uses the full correlation structure of the model. The "approximate" method makes the simplifying assumption that the study has a mean of m samples in each of k groups, and computes m based on the study design.  When the study design is evenly balanced (i.e. the assumption is met), this gives the same results as the "full" method.  
 #' 
 #' @return
 #' effective sample size for each random effect in the model
@@ -18,8 +18,8 @@
 #' Liu, G., and Liang, K. Y. (1997). Sample size calculations for studies with correlated observations. Biometrics, 53(3), 937-47.
 #'
 #' "full" method: if V_x = var(Y;x) is the variance-covariance matrix of Y, the response, based on the covariate x, then the effective sample size corresponding to this covariate is \\Sigma_\{i,j\} (V_x^\{-1\})_\{i,j\}.  In R notation, this is: sum(solve(V_x)).  In practice, this can be evaluted as sum(w), where R %*% w == One and One is a column vector of 1's
-#'
-#' "fast" method: takes advantage of the fact that the eigen decompostion of a sparse, low rank, symmetric matrix. May be faster for large datasets.
+#
+# "fast" method: takes advantage of the fact that the eigen decompostion of a sparse, low rank, symmetric matrix. May be faster for large datasets.
 #'
 #' "approximate" method: Letting m be the mean number of samples per group, k be the number of groups, and rho be the intraclass correlation, the effective sample size is m*k / (1+rho*(m-1))
 #'
@@ -49,7 +49,7 @@ setGeneric("ESS", signature="fit",
 setMethod("ESS", "lmerMod",
 	function( fit, method="full" ){
 
-		if( !(method %in% c("full", "fast", "approximate")) ){
+		if( !(method %in% c("full", "approximate")) ){
 			stop(paste("method is not valid:", method))
 		}
 
@@ -92,6 +92,7 @@ setMethod("ESS", "lmerMod",
 
 				}
 				# Disabled in this version 
+				# "fast" is exact and can be much faster for > 500 samples.
 				# else{
 				# 	# November 22, 2016
 				# 	A = sigG$G[[i]] * fraction
