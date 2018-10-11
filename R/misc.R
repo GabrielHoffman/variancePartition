d6396fa70c4a786a2c2683a67f7205aa6485ffb2
GabrielHoffman <gabrielhoffman@typhon.wireless.mountsinai.org>
2018-10-11 13:38:43 -0400

    Add to master

--- R/misc.R
+++ R/misc.R
@@ -157,6 +157,88 @@ function(x, row.names = NULL, optional = FALSE,...)
 #' @name varPartData
 NULL
 
+
+#' Simulation dataset for examples
+#'
+#' A simulated dataset of gene counts
+#'
+#' \itemize{    
+#'	\item geneCounts gene expression in the form of RNA-seq counts
+#'	\item geneExpr gene expression on a continuous scale
+#'  \item info metadata about the study design
+#' }
+#' @docType data
+#' @keywords datasets
+#' @usage data(varPartData)
+#' @format A dataset of 100 samples and 200 genes
+#' @name varPartData
+"geneCounts"
+
+#' Simulation dataset for examples
+#'
+#' info about study design
+#'
+#' \itemize{    
+#'	\item geneCounts gene expression in the form of RNA-seq counts
+#'	\item geneExpr gene expression on a continuous scale
+#'  \item info metadata about the study design
+#' }
+#' @docType data
+#' @keywords datasets
+#' @usage data(varPartData)
+#' @format A dataset of 100 samples and 200 genes
+#' @name varPartData
+"info"
+
+
+#' Simulation dataset for examples
+#'
+#' Normalized expression data
+#'
+#' \itemize{    
+#'	\item geneCounts gene expression in the form of RNA-seq counts
+#'	\item geneExpr gene expression on a continuous scale
+#'  \item info metadata about the study design
+#' }
+#' @docType data
+#' @keywords datasets
+#' @usage data(varPartData)
+#' @format A dataset of 100 samples and 200 genes
+#' @name varPartData
+"geneExpr"
+
+#' Simulation dataset for dream example
+#'
+#' Gene counts from RNA-seq
+#'
+#' \itemize{    
+#'	\item countMatrix gene expression in the form of RNA-seq counts
+#'  \item metadata metadata about the study design
+#' }
+#' @docType data
+#' @keywords datasets
+#' @usage data(varPartDEdata)
+#' @format A dataset of 24 samples and 19,364 genes
+#' @name varPartDEdata
+"countMatrix"
+
+#' Simulation dataset for dream example
+#'
+#' metadata matrix of sample information
+#'
+#' \itemize{    
+#'	\item countMatrix gene expression in the form of RNA-seq counts
+#'  \item metadata metadata about the study design
+#' }
+#' @docType data
+#' @keywords datasets
+#' @usage data(varPartDEdata)
+#' @format A dataset of 24 samples and 19,364 genes
+#' @name varPartDEdata
+"metadata"
+
+
+
 #' Default colors for ggplot
 #' 
 #' Return an array of n colors the same as the default used by ggplot2
