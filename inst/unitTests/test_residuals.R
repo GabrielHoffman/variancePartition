885494327d2a49a8ce012598888c1e2f8b763b96
GabrielHoffman <gabrielhoffman@typhon.fios-router.home>
2020-01-17 12:40:13 -0500

    Add new unit tests

---
+++ inst/unitTests/test_residuals.R
@@ -0,0 +1,41 @@
+
+library(RUnit)
+
+test_residuals = function(){
+		
+	# q()
+	#  R
+	#  library(variancePartition)
+	library(lme4)
+	# residuals
+	data(varPartData)
+
+	# Compute dream to lmer
+	#######################
+
+	# compute residuals with dream
+	form <- ~ Batch + (1|Individual) + (1|Tissue)
+	fit = dream( geneExpr[1:10,], form, info, computeResiduals=TRUE)
+	res = residuals(fit, geneExpr[1:10,])
+
+	# compute with lmer
+	fit1 = lmer(geneExpr[1,] ~ Batch + (1|Individual) + (1|Tissue), info)
+
+	check1 = checkEquals( res[1,], residuals(fit1))
+
+	# Compute dream to lmFit
+	#######################
+	
+	# compute residuals with dream
+	form <- ~ Batch 
+	fit = dream( geneExpr[1:10,], form, info)
+	res = residuals(fit, geneExpr[1:10,])
+
+	# compute with lmFit
+	fitLM = lmFit( geneExpr[1:10,], model.matrix(form, info))
+
+	check2 = checkEquals( res, residuals(fitLM, geneExpr[1:10,]))
+
+	check1 & check1
+}
+
---
+++ inst/unitTests/test_sparseMatrix.R
@@ -0,0 +1,115 @@
+
+# voomWithDreamWeights
+
+library(RUnit)
+
+test_vp_sparseMatrix = function(){
+		
+	library(BiocParallel)
+	library(Matrix)
+	register(SerialParam(), default=TRUE)
+
+	data(varPartData)
+	set.seed(1)
+
+	# select entries to make zero so that matrix is sparse
+	idx = sample(length(geneExpr), length(geneExpr)*.3, replace=FALSE)
+	geneExpr[idx] = 0
+
+	# convert to sparse matrix
+	geneSparse = as(geneExpr, "sparseMatrix")
+
+	# get fill density
+	length(geneSparse@x) / length(geneExpr)
+
+
+	form <- ~ Age + (1|Individual) + (1|Tissue)
+
+	# test fitExtractVarPartModel on sparse data
+	vp <- fitExtractVarPartModel( geneExpr[1:10,], form, info )
+	vp_sparse <- fitExtractVarPartModel( geneSparse[1:10,], form, info )
+
+	res1 = checkEquals(vp, vp_sparse)
+
+	# test fitVarPartModel on sparse data
+	varPart <- fitVarPartModel( geneExpr[1:10,], form, info )
+	varPart_sparse <- fitVarPartModel( geneSparse[1:10,], form, info )
+
+	res2 = checkEquals(coef(varPart[[1]]), coef(varPart_sparse[[1]]))
+
+	# dream
+	data(varPartData)
+
+	form <- ~ Batch + (1|Individual) + (1|Tissue)
+
+	fit = dream( geneExpr[1:10,], form, info)
+	# topTable( fit )
+
+	fit_sparse = dream( geneSparse[1:10,], form, info)
+	# topTable( fit_sparse )
+
+	res3 = checkEquals( topTable( fit ), topTable( fit_sparse ))
+
+	res1 & res2 & res3
+}
+
+
+
+
+# library(variancePartition)
+# library(BiocParallel)
+# library(Matrix)
+# # register(SerialParam(), default=TRUE)
+
+# data(varPartData)
+# set.seed(1)
+
+# # select entries to make zero so that matrix is sparse
+# idx = sample(length(geneExpr), length(geneExpr)*.9, replace=FALSE)
+# geneExpr[idx] = 0
+
+# # convert to sparse matrix
+# geneSparse = as(geneExpr, "sparseMatrix")
+
+# geneSparse = rbind(geneSparse, geneSparse, geneSparse, geneSparse)
+# geneSparse = rbind(geneSparse, geneSparse, geneSparse, geneSparse)
+# geneSparse = rbind(geneSparse, geneSparse, geneSparse, geneSparse)
+# geneSparse = cbind(geneSparse, geneSparse, geneSparse, geneSparse)
+# geneSparse = cbind(geneSparse, geneSparse, geneSparse, geneSparse)
+# geneSparse = cbind(geneSparse, geneSparse, geneSparse, geneSparse)
+# # geneSparse = cbind(geneSparse, geneSparse, geneSparse, geneSparse)
+# # geneSparse = rbind(geneSparse, geneSparse, geneSparse, geneSparse)
+# # geneSparse = rbind(geneSparse, geneSparse, geneSparse, geneSparse)
+
+# info = rbind(info, info, info, info)
+# info = rbind(info, info, info, info)
+# info = rbind(info, info, info, info)
+
+# rownames(geneSparse) = 1:nrow(geneSparse)
+# colnames(geneSparse) = 1:ncol(geneSparse)
+
+# dim(geneSparse)
+
+# # get fill density
+# length(geneSparse@x) / (nrow(geneSparse)*ncol(geneSparse))
+
+# v = apply(geneSparse, 1, var)
+
+# form <- ~ Age# + (1|Batch)
+
+# vp <- fitExtractVarPartModel( as.matrix(geneSparse[v>0.7,]), form, info )
+
+
+# vp_sparse <- fitExtractVarPartModel( geneSparse[v>0.7,], form, info )
+
+
+# identical(vp, vp_sparse)
+
+
+
+
+
+
+
+
+
