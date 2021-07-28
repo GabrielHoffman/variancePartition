6eef49a9212e7264a314fb5febea87c6f9930d4d
Gabriel Hoffman <gabrielhoffman@glia.local>
2021-07-28 15:44:00 -0400

    fix bug in rdf

--- R/rdf_functions.R
+++ R/rdf_functions.R
@@ -70,7 +70,7 @@ rdf_from_matrices = function(A,B){
 	# catch this error and drop very small values
 	tryCatch({
 		dcmp_A <- eigen(tcrossprod(A))
-		}, error = function(){
+		}, error = function(e){
 			dcmp_A <- eigen(tcrossprod(drop0(A, tol=1e-12)))
 		})
 
