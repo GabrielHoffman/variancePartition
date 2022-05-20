acc296d48ad61cbdf17a542e0e8bd4f7ca490858
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2022-05-20 13:56:56 -0400

    1.26.1

--- R/data_annotation.R
+++ R/data_annotation.R
@@ -16,31 +16,31 @@
 NULL
 
 
-#' Simulation dataset for examples
-#'
-#' A simulated dataset of gene counts
-#'
-#' \itemize{    
-#'	\item geneCounts gene expression in the form of RNA-seq counts
-#'	\item geneExpr gene expression on a continuous scale
-#'  \item info metadata about the study design
-#' }
+##' Simulation dataset for examples
+##'
+##' A simulated dataset of gene counts
+##'
+##' \itemize{    
+##'	\item geneCounts gene expression in the form of RNA-seq counts
+##'	\item geneExpr gene expression on a continuous scale
+##'  \item info metadata about the study design
+##' }
 #' @docType data
 #' @keywords datasets
-#' @usage data(varPartData)
+##' @usage data(varPartData)
 #' @format A dataset of 100 samples and 200 genes
 #' @name varPartData
 "geneCounts"
 
-#' Simulation dataset for examples
-#'
-#' info about study design
-#'
-#' \itemize{    
-#'	\item geneCounts gene expression in the form of RNA-seq counts
-#'	\item geneExpr gene expression on a continuous scale
-#'  \item info metadata about the study design
-#' }
+##' Simulation dataset for examples
+##'
+##' A simulated dataset of gene counts
+##'
+##' \itemize{    
+##'	\item geneCounts gene expression in the form of RNA-seq counts
+##'	\item geneExpr gene expression on a continuous scale
+##'  \item info metadata about the study design
+##' }
 #' @docType data
 #' @keywords datasets
 #' @usage data(varPartData)
@@ -49,48 +49,49 @@ NULL
 "info"
 
 
-#' Simulation dataset for examples
-#'
-#' Normalized expression data
-#'
-#' \itemize{    
-#'	\item geneCounts gene expression in the form of RNA-seq counts
-#'	\item geneExpr gene expression on a continuous scale
-#'  \item info metadata about the study design
-#' }
+
+##' Simulation dataset for examples
+##'
+##' A simulated dataset of gene counts
+##'
+##' \itemize{    
+##'	\item geneCounts gene expression in the form of RNA-seq counts
+##'	\item geneExpr gene expression on a continuous scale
+##'  \item info metadata about the study design
+##' }
 #' @docType data
 #' @keywords datasets
-#' @usage data(varPartData)
+##' @usage data(varPartData)
 #' @format A dataset of 100 samples and 200 genes
 #' @name varPartData
 "geneExpr"
 
-#' Simulation dataset for dream example
-#'
-#' Gene counts from RNA-seq
-#'
-#' \itemize{    
-#'	\item countMatrix gene expression in the form of RNA-seq counts
-#'  \item metadata metadata about the study design
-#' }
+##'
+##' A simulated dataset of gene counts
+##'
+##' \itemize{    
+##'	\item geneCounts gene expression in the form of RNA-seq counts
+##'	\item geneExpr gene expression on a continuous scale
+##'  \item info metadata about the study design
+##' }
 #' @docType data
 #' @keywords datasets
-#' @usage data(varPartDEdata)
+##' @usage data(varPartData)
 #' @format A dataset of 24 samples and 19,364 genes
 #' @name varPartDEdata
 "countMatrix"
 
-#' Simulation dataset for dream example
-#'
-#' metadata matrix of sample information
-#'
-#' \itemize{    
-#'	\item countMatrix gene expression in the form of RNA-seq counts
-#'  \item metadata metadata about the study design
-#' }
+##'
+##' A simulated dataset of gene counts
+##'
+##' \itemize{    
+##'	\item geneCounts gene expression in the form of RNA-seq counts
+##'	\item geneExpr gene expression on a continuous scale
+##'  \item info metadata about the study design
+##' }
 #' @docType data
 #' @keywords datasets
-#' @usage data(varPartDEdata)
+##' @usage data(varPartData)
 #' @format A dataset of 24 samples and 19,364 genes
 #' @name varPartDEdata
 "metadata"
